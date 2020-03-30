fcb038081f3fb050edae761d5a124d7b06c6a524
pioncz <pioncz@gmail.com>
2020-03-30 17:02:28 +0200

    store init, db init

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import routes from './routes';
 import App from './App.vue';
+import routes from './routes';
+import store from './store';
 
 Vue.use(VueRouter);
 
@@ -14,6 +15,7 @@ const router = new VueRouter({
 const app = new Vue({
   el: '#app',
   router,
+  store,
   render: (h) => h(App),
 });
 
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,12 +1,21 @@
 <template>
   <div class="hello">
     <h1>Home</h1>
+    <button v-on:click="dd">Hi</button>
   </div>
 </template>
 
 <script>
+import store from '../store';
+
 export default {
   name: 'Home',
+  methods: {
+    dd() {
+      console.log('x');
+      store.commit('increment');
+    },
+  },
 };
 </script>
 
