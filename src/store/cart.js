40222dd9df1b6f7f570c87bc9ee27546b0d0e0b5
pioncz <pioncz@gmail.com>
2020-05-07 19:22:26 +0200

    cart wip

---
+++ src/store/cart.js
@@ -0,0 +1,22 @@
+import * as actionTypes from './action-types';
+import * as mutationTypes from './mutation-types';
+import * as getterTypes from './getter-types';
+
+const cart = {
+  state: {
+    cartList: [],
+    cartLoading: false,
+    cartError: null,
+  },
+  mutations: {
+
+  },
+  getters: {
+
+  },
+  actions: {
+
+  },
+};
+
+export default cart;
